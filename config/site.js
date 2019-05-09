6fd6a2b9959f6703de17f4ce357bd30f35059c66
Res Yeg <shaqk702@gmail.com>
2019-05-08 20:24:55 -0700

    udpate config data

--- config/site.js
+++ config/site.js
@@ -1,21 +1,21 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Emilia', // Navigation and Site Title
-  siteTitleAlt: 'Emilia - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Emilia', // short_name for manifest
+  siteTitle: 'Pixart', // Navigation and Site Title
+  siteTitleAlt: 'Pixart by NamediaDigital', // Alternative Site title for SEO
+  siteTitleShort: 'Pixart', // short_name for manifest
   siteHeadline: 'Publishing & Creating stunning photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://emilia.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://pixart.namedia.digital', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
   author: 'LekoArts', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@emilia', // Twitter Username
-  ogSiteName: 'emilia', // Facebook Site Name
+  // userTwitter: '@namediadigital', // Twitter Username
+  // ogSiteName: 'emilia', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
-  googleAnalyticsID: 'UA-47519312-4',
+  // googleAnalyticsID: 'UA-47519312-4',
 
   // Manifest and Progress color
   themeColor: '#3498DB',
@@ -23,20 +23,20 @@ module.exports = {
 
   // Your information
   avatar: '/logos/lekoarts.png',
-  name: 'LekoArts',
-  location: 'Germany',
+  name: 'Pixart by NamediaDigital',
+  location: 'USA',
   socialMedia: [
+    // {
+    //   url: 'https://twitter.com/lekoarts_de',
+    //   name: 'Twitter',
+    // },
     {
-      url: 'https://twitter.com/lekoarts_de',
-      name: 'Twitter',
-    },
-    {
-      url: 'https://www.instagram.com/lekoarts.de',
+      url: 'https://www.instagram.com/serjay.pixart/',
       name: 'Instagram',
     },
-    {
-      url: 'https://www.lekoarts.de/en',
-      name: 'Homepage',
-    },
+    // {
+    //   url: 'https://www.lekoarts.de/en',
+    //   name: 'Homepage',
+    // },
   ],
 }
